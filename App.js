9ad3408713184cf63379ce9a089cbc10afd43337
PAULO ROBERTO BELEM DE SEIXAS <61366935268@professores.estacio.br>
2024-03-24 18:40:31 -0300

    Tela de login pupfeeder

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View,Image, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return ( 
+     
+      <View style={styles.container}>
+          <Image
+            source={require('./assets/pupfeeder_logo.png')} 
+            style = {styles.logo}          
+          />
+        <TextInput
+          placeholder='Email'
+          style = {styles.entradas}
+        />
+         <TextInput
+          placeholder='Senha'
+          style = {styles.entradas}
+        />
+        <Button
+          title='Acessar'
+          style = {styles.botoes}
+        />
+        <TouchableOpacity
+          style = {styles.opacidade}
+        > 
+          <Text>Esqueci a Senha</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style = {styles.opacidade}
+        >
+          <Text>Criar uma conta</Text>
+        </TouchableOpacity>
+        
+      </View>
+   
   );
 }
 
@@ -17,4 +44,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    flex: 0.3,     
+    resizeMode: 'contain'
+  },
+  entradas:{
+    borderColor: 'black',
+    margin:10,
+    padding:20,
+    borderWidth: 1,
+    width: "80%",
+    borderRadius: 7,
+    height: 50
+  },
+  botoes:{
+   backgroundColor: 'yellow',
+   margin:10
+  },
+  opacidade:{
+    borderColor: 'black',
+    margin:10,
+    width: "80%",
+    height: 50
+  }
 });
